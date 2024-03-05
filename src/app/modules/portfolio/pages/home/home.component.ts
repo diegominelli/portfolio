18cfa4421422d828a94d2d963afb7c3f42f9c840
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-03-05 18:31:55 -0300

    criacao e desenvolvimento do componente knowledge e criacao da interface

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,10 +1,11 @@
+import { KnowledgeComponent } from './../../components/knowledge/knowledge.component';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, KnowledgeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
