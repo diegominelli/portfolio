f5f5543ef7a7782a3b2502aa05f06425951e8dfd
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-03-15 19:55:17 -0300

    criacao e desenvolvimento do dialog

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPainelClass } from '../../enum/EDialogPainelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/vfull.png',
@@ -25,80 +30,12 @@ export class ProjectsComponent {
         },
       ],
     },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto FullStack',
-      title: 'Vida FullStack',
-      with: '100px',
-      height: '51px',
-      description:
-        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto FullStack',
-      title: 'Vida FullStack',
-      with: '100px',
-      height: '51px',
-      description:
-        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto FullStack',
-      title: 'Vida FullStack',
-      with: '100px',
-      height: '51px',
-      description:
-        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto FullStack',
-      title: 'Vida FullStack',
-      with: '100px',
-      height: '51px',
-      description:
-        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
-    {
-      src: 'assets/img/projects/vfull.png',
-      alt: 'Projeto FullStack',
-      title: 'Vida FullStack',
-      with: '100px',
-      height: '51px',
-      description:
-        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular</p>',
-      links: [
-        {
-          name: 'Conheça o Blog',
-          href: 'https://vidafullstack.com.br',
-        },
-      ],
-    },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPainelClass.PROJECTS,
+    });
+  }
 }
